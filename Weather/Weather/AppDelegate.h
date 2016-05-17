48c73f1a3ca5349b1803b9b35c84b7490dd9b90d
Badteddy <334675325@qq.com>
2016-05-17 17:10:46 +0800

    Primary

---
+++ Weather/Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by King on 16/5/17.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
